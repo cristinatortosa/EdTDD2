524655c563c5ff8d1300ff409a024e9b90595e90
Minipica <tortosarodriguezcristina@gmail.com>
2023-03-12 16:55:26 +0100

    Refactorizo la clase Test a una carpeta Test porque por algun motivo se me olvidó crearla

--- Tareas-TDD/Test/FizzBuzzTest.java
+++ Tareas-TDD/Test/FizzBuzzTest.java
@@ -1,6 +1,5 @@
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
 
 class FizzBuzzTest {
     @Test
