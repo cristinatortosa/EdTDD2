89eac0a20427846827950c387e8e7fcdc0729a40
Minipica <tortosarodriguezcristina@gmail.com>
2023-03-14 23:04:31 +0100

    Punto 4, test c y d de la clase calculadora

--- Tarea-TDD2-Calculadora/Test/OperacionesTDDTest.java
+++ Tarea-TDD2-Calculadora/Test/OperacionesTDDTest.java
@@ -5,12 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class OperacionesTDDTest {
     @Test
     void testAlNoIntroducirUnNumeroDevuelve0() {
-        //Assert
+        //Assert                //Act
         assertEquals(0, OperacionesTDD.suma(""));
     }
     @Test
     void testAlIntroducir1Devuelve1() {
-        //Assert
+        //Assert                //Act
         assertEquals(1, OperacionesTDD.suma("1"));
+    }
+    @Test
+    void testAlIntroducir1y2Devuelve3() {
+        //Assert                //Act
+        assertEquals(3, OperacionesTDD.suma("1, 2"));
+    }
+    @Test
+    void testAlIntroducir1y1yy2Devuelve4() {
+        //Assert                //Act
+        assertEquals(4, OperacionesTDD.suma("1, 1, 2"));
     }
 }
--- Tarea-TDD2-Calculadora/src/OperacionesTDD.java
+++ Tarea-TDD2-Calculadora/src/OperacionesTDD.java
@@ -1,9 +1,12 @@
 public class OperacionesTDD {
     public static int suma(String numero) {
-        if (numero.isEmpty()) {
-            return 0;
-        } else {
-            return 1;
+        int suma = 0;
+        String[] numeros = numero.split("\\D+");
+        for (int i = 0; i < numeros.length; i++) {
+            if (!numero.isEmpty()) {
+                suma += Integer.parseInt(numeros[i]);
+            }
         }
+        return suma;
     }
 }
